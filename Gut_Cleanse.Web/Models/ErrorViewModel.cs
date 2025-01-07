eeb4a835014545e4ca7acb7655b721967c84ed4e
simranjeetsa <simranjeetsa@gmail.com>
2025-01-07 16:59:31 +0530

    Add project files.

---
+++ Gut_Cleanse.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gut_Cleanse.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
