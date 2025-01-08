c174ff124833f382c33c9a388cd6631e735709e1
Prikshit-Kamal <prikshit.sa@outlook.com>
2025-01-08 15:53:29 +0530

    changes

---
+++ Gut_Cleanse.Web/Controllers/ProgramsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gut_Cleanse.Web.Controllers
+{
+    public class ProgramsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("programs/one-one-gut-reset-revolution")]
+        public IActionResult OneOneGutResetRevolution()
+        {
+            return View();
+        }
+
+        [Route("programs/gut-and-glory")]
+        public IActionResult GutAndGlory()
+        {
+            return View();
+        }
+
+        [Route("programs/gut-intelligence-workshop")]
+        public IActionResult GutIntelligenceWorkshop()
+        {
+            return View();
+        }
+    }
+}
