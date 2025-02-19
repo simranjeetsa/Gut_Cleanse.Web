43d6003f1c5a65e86c36e7b66d4aaa625aeed022
simranjeetsa <simranjeetsa@gmail.com>
2025-02-19 14:07:25 +0530

    changes

---
+++ Gut_Cleanse.Data/Migrations/20250219083644_Add_entry_in_aspnetrole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gut_Cleanse.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_entry_in_aspnetrole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "1ace2763-ee9c-11ef-872f-bc2411ffd154", null, "Customer", "CUSTOMER" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1ace2763-ee9c-11ef-872f-bc2411ffd154");
+        }
+    }
+}
